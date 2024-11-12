b1d01efdd3bd8bbca280f59e091d19f0e3cc51bf
hwangjuhyeon <juhyeon5318@gmail.com>
2024-11-12 15:21:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ st.write("만나서 반갑습니다.")
 
 st.link_button("유튜브 바로가기","https://www.youtube.com")
 st.link_button("네이버 바로가기","https://www.naver.com")
+st.link_button("구글 바로가기","https://www.google.com")
 
 import openai
