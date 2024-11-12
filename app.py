ee01890499b03fd9933e4d0035014f7bfcba1fc3
hwangjuhyeon <juhyeon5318@gmail.com>
2024-11-12 15:11:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ st.header("안녕하세요")
 st.write("만나서 반갑습니다.")
 
 st.link_button("유튜브 바로가기","https://www.youtube.com")
-st.link_button("네이버 바로가기","naver.com")
+st.link_button("네이버 바로가기","https://www.naver.com")
