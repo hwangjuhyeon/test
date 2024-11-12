3e9fbebe09c11430bae3180f8350bf8026ebebc1
hwangjuhyeon <juhyeon5318@gmail.com>
2024-11-12 15:20:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,3 @@ st.link_button("유튜브 바로가기","https://www.youtube.com")
 st.link_button("네이버 바로가기","https://www.naver.com")
 
 import openai
-openai.api_key="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"
-
-completion = openai.ChatCompletion.create(
-  model="gpt-4o-mini", 
-  messages=[{"role": "user", "content": "Who won the world series in 2020?"}]
-)
-
-print(completion.choices[0].message.content)
