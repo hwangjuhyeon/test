3a56547e394e62c78623c28f29d61f4499b824aa
hwangjuhyeon <juhyeon5318@gmail.com>
2024-11-13 14:32:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,31 +13,7 @@ api=st.text_input("API_KEY")
 client=OpenAI
 client.api_key=api
 
-def ask_openai(question, chat_log=None):
-    if chat_log is None:
-        chat_log = []
-    messages = [{"role": "user", "content": msg} for msg in chat_log]
-    messages.append({"role": "user", "content": question})
-    # new openai 반영.
-    response = client.chat.completions.create(
-        model="gpt-4.o-mini",
-        messages=messages
-    )
-    # new openai 반영.
-    answer = response.choices[0].message.content
-    return answer
+if 'key' not in st.session_state:
+    st.session_state['key'] = 'value'
 
-def chat():
-    st.write("chatting is started. If you want to exit, enter 'exit'.")
-    chat_log = []
-    while True:
-        user_input = st.text_input("User: ")
-        if user_input.lower() == 'exit':
-            st.write("Exiting...")
-            break
-        response = ask_openai(user_input, chat_log)
-        st.write(f"AI bot: {response}")
-        chat_log.extend([user_input, response])
-
-if __name__ == "__main__":
-    chat()
+st.write(st.session_state.key)
