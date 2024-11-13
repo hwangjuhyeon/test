036e0b9f2aad770e2466f0f1374055bc1a76ed4e
hwangjuhyeon <juhyeon5318@gmail.com>
2024-11-13 15:22:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,34 @@ st.session_state.name
 st.page_link("https://www.google.com", label="Google", icon="🌎")
 
 api_key = st.text_input("OpenAI API Key")
+
+prompt = st.text_area("Prompt")
+messages = [
+    {"role": "user", "content": prompt}
+]
+
+
+answer=''
+
+if st.button("Generate"):
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages
+    )
+    answer = response.choices[0].message.content
+
+st.text(answer)
+
+image_url=''
+
+if st.button("image"):
+    response=client.images.generate(
+        model="dell-e-3",
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    image_url=response.data[0].url
+
+if image_url:
+    st.markdown(f"![{prompt}]({image_url})")
